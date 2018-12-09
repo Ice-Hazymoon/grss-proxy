6e1e534e85920955d8dae4e5fa57c70b36cdff25
Hazymoon <imiku.me@gmail.com>
2018-12-10 00:31:46 +0800

    update

--- index.js
+++ index.js
@@ -308,6 +308,6 @@ app.use(function(err, req, res, next) {
     res.status(500).send('Something broke!');
 });
 
-app.listen('3000', () => {
+app.listen('80', () => {
     console.log('代理已启动');
 });
